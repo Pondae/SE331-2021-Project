3de688b2af29a7958fc82ded6e9923b4d19c1e5b
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-08-12 15:49:39 +0700

    create graph

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import Gstore from "./store";
 import "nprogress/nprogress.css";
+import VueChartkick from "vue-chartkick";
+import "chartkick/chart.js";
 
-createApp(App).use(router).provide("Global_Store", Gstore).mount("#app");
+createApp(App)
+  .use(VueChartkick)
+  .use(router)
+  .provide("Global_Store", Gstore)
+  .mount("#app");
