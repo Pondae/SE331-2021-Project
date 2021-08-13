522ca5622f0703966ec43f5bf1a2e8502d532196
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2021-08-13 23:39:41 +0700

    edit footer problem #3

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -7,6 +7,7 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
   />
+  <br><br><br>
   <div class="footer-dark" >
     <footer>
       <div class="container"> 
